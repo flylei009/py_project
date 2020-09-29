ef1feb1231209ec797788460141ac83df00ef177
yanjp <yanjp@feewee.cn>
2020-09-29 15:39:59 +0800

    学习20200929

--- exercise/猫眼电影排行不用正则.py
+++ exercise/猫眼电影排行不用正则.py
@@ -27,10 +27,48 @@ def main(offset):
     html = file1.read()
     soup = BeautifulSoup(html, 'lxml')
     # print(soup.text)
-    print(soup.find_all('dd'))
 
-    for link in soup.find_all('a'):
+    ddList = soup.find_all('dd')
+    print(ddList)
+    newList = [];
+    for dd in ddList:
+        newDic = {}
+        # 获取title属性
+        newDic['id'] = dd.i.string
+        # 获取title属性
+        # newDic['title'] = dd.a.attrs['title']
+        newDic['title'] = dd.a['title']   #一个节点多个属性的，获取某个属性的值
+        # 获取图片
+        imgList = dd.find_all('img')
+        for imgLabel in imgList:
+            if 'data-src' not in imgLabel.attrs:
+                continue
+            else:
+                newDic['img'] = imgLabel['data-src']
+        # 获取P标签
+        pList = dd.find_all('p')
+        for pLabel in pList:
+            print(pLabel)
+            if pLabel.attrs['class'] == ['star']:
+                newDic['actor'] = pLabel.string.replace('\n                ','').replace('\n        ','')
+            elif pLabel.attrs['class'] == ['releasetime']:
+                newDic['time'] = pLabel.string.replace('上映时间：', '')
+            elif pLabel.attrs['class'] == ['score']:
+                score = ''
+                scoreList = pLabel.find_all('i')
+                for zpLabel in scoreList:
+                    score= score+zpLabel.string
+                newDic['score'] = score
+            else:
+                continue
 
+        # newDic['actor'] = imgLabel['data-src']
+
+        # newDic['img'] = dd.img.attrs['src']
+        # newDic['actors'] = dd.img('data-src')
+        newList.append(newDic)
+
+    print("newList",newList)
 
 #     print(link.get('href'))
 
